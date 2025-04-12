88326277b7f8196e99a72bf4099a570ee2133f34
Maira Bermeo <113212501+mairabermeo@users.noreply.github.com>
2025-04-12 17:38:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-/*
-Maira Bermeo - changes it to 'ejs' and also downloaded 'ejs'
-*/
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const questionsRouter = require('./routes/questions'); // Import the questions route
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/quiz', questionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,11 +30,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
