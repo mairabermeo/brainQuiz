eeb0cbab5845a5fe8176d27555b415037ee4d043
Maira Bermeo <113212501+mairabermeo@users.noreply.github.com>
2025-04-12 17:16:34 -0600

    Update loading.js

--- public/javascripts/loading.js
+++ public/javascripts/loading.js
@@ -1,6 +1,6 @@
 // Author: Maira Bermeo
 window.onload = function () {
     setTimeout(function () {
-        window.location.href = "/quiz";  // Redirects to /quiz route
-    }, 4000);  // Delay of 4 seconds
+        window.location.href = "/quiz"; 
+    }, 4000); 
 };
