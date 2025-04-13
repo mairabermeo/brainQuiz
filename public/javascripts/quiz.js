2d54ee81cc0faedc5b28ce2ffcb59a2255d90431
Maira Bermeo <113212501+mairabermeo@users.noreply.github.com>
2025-04-12 18:54:40 -0600

    Update quiz.js

--- public/javascripts/quiz.js
+++ public/javascripts/quiz.js
@@ -14,7 +14,7 @@ let allQuestions = [];
 let selectedQuestions = [];
 let userAnswers = [];
 let currentQuestionIndex = 0;
-let totalTimeLeft = 30; 
+let totalTimeLeft = 45; 
 let timerInterval;
 
 // Element references
